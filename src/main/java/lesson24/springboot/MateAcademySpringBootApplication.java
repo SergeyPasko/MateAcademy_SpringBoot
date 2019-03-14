c98f81914b2bc4b071b8fb5d424b0ec9617e6d31
spasko <spasko@smc-pasko.innovecs.local>
2019-03-14 15:46:13 +0200

    Init commit

---
+++ src/main/java/lesson24/springboot/MateAcademySpringBootApplication.java
@@ -0,0 +1,13 @@
+package lesson24.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateAcademySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateAcademySpringBootApplication.class, args);
+	}
+
+}
